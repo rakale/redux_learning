251610156e978d3c7bb774b3e8c264d4413377b4
Raiko Kalev <rakale@ttu.ee>
2021-04-07 14:56:31 +0300

    more advanced redux, devtools, actiontypes

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import * as actionTypes from '../../store/actions';
 import {connect} from 'react-redux';
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import * as actionCreators from '../../store/actions/index';
 
 class Counter extends Component {
 
@@ -36,12 +36,12 @@ const mapStateToProps = state =>{
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
-        onAddCounter: () => dispatch({type: actionTypes.ADD, val: 10}),
-        onSubtractCounter: () => dispatch({type: actionTypes.SUBTRACT, val: 15}),
-        onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
-        onStoreResult: (result) => dispatch({type: actionTypes.STORE_RESULT, result: result}),
-        onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultElId:id})
+        onIncrementCounter: () => dispatch(actionCreators.increment()),
+        onAddCounter: () => dispatch(actionCreators.add(10)),
+        onSubtractCounter: () => dispatch(actionCreators.subtract(15)),
+        onDecrementCounter: () => dispatch(actionCreators.decrement()),
+        onStoreResult: (result) => dispatch(actionCreators.storeResult(result)),
+        onDeleteResult: (id) => dispatch(actionCreators.deleteResult(id))
 
     };
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
 import {Provider} from 'react-redux';
 import './index.css';
 import App from './App';
@@ -13,7 +14,20 @@ const rootReducer = combineReducers({
     res: resultReducer
 });
 
-const store = createStore(rootReducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('MIDDLEWARE || DISPATCHING ', action);
+            const result = next(action);
+            console.log('MIDDLEWARE next state', store.getState());
+            return result;
+        }
+    }
+};
+
+const composeEnchancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnchancers(applyMiddleware(logger, thunk)));
 
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
